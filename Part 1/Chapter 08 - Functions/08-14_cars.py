f3b42dbcb652529cc375612aa70844cfa94aefce
Lucas Dondo <lucasdondo203@gmail.com>
2023-03-31 13:22:21 -0300

    Renamed files.

--- Part 1/Chapter 08 - Functions/08-14_cars.py
+++
@@ -1,9 +0,0 @@
-def create_car(manufacturer, model_name, **car):  # "**car" creates the dic. and
-    ''' Make a dic. representing a car. '''      # assigns the kwargs. to it.
-    car['manufacturer'] = manufacturer           # Then the other args are
-    car['model_name'] = model_name               # inserted into it.
-    return car
-
-
-car = create_car('honda', 'pilot', size='big', location='argentina')
-print(car)
