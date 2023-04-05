6a7a8894b6593903261a9d7cf2ec60ae8900bafc
Lucas Dondo <lucasdondo203@gmail.com>
2023-04-05 12:37:51 -0300

    M1 13 Created messy_rain. Closes #16.

---
+++ Part 2/Project 1. Alien Invasion/Chapter 13. Aliens!/messy_rain/raindrop.py
@@ -0,0 +1,38 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Raindrop(Sprite):
+    ''' An attempt to represent a raindrop. '''
+
+    def __init__(self, sr):
+        ''' Initialize main attributes. '''
+
+        super().__init__()
+
+        self.screen = sr.screen
+        self.screen_rect = sr.screen_rect
+        self.settings = sr.settings
+
+        self.image = pygame.image.load('images/raindrop.png')
+        self.rect = self.image.get_rect()
+
+        # Start each new raindrop at the top of the screen, not visible.
+        self.rect.bottom = self.screen_rect.top
+
+    def fell(self):
+        ''' Returns if the raindrop is falling or if it already fell. '''
+    
+        while self.rect.top >= self.screen_rect.bottom:
+            return True
+
+    def update(self):
+        ''' Updates the raindrop position. '''
+        
+        self.y = float(self.rect.y)    
+        self.y += self.settings.raindrop_speed
+        self.rect.y = self.y
+
+    def blitme(self):
+        ''' Blits the raindrop. '''
+    
+        self.screen.blit(self.image, self.rect)
