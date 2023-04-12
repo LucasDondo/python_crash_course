15b1dedab64480e572675f8bf234955deff45c52
Lucas Dondo <lucasdondo203@gmail.com>
2023-04-12 11:11:37 -0300

    M2 Fixed #20.

--- Part 2/Project 1. Alien Invasion/alien_invasion_by_me/alien_invasion.py
+++ Part 2/Project 1. Alien Invasion/alien_invasion_by_me/alien_invasion.py
@@ -292,9 +292,9 @@ class AlienInvasion():
         ''' Update images on the screen, and flip to the new screen. '''
 
         self.screen.fill(self.bg_color)
-        self.rocket.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.rocket.blitme()
         self.aliens.draw(self.screen)
 
         # Draw the score information.
