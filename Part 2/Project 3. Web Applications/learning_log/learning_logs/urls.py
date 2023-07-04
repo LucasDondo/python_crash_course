d9f93a774a0e4888ab6626d61704f0d1d0551793
Lucas Dondo <lucasdondo203@gmail.com>
2023-07-04 11:56:33 -0300

    M4#68 Up to 19.1. Users can now log in and out.

--- Part 2/Project 3. Web Applications/learning_log/learning_logs/urls.py
+++ Part 2/Project 3. Web Applications/learning_log/learning_logs/urls.py
@@ -8,6 +8,16 @@ urlpatterns = [
     path("", views.index, name="index"),
     # All topics' page:
     path("topics/", views.topics, name="topics"),
+    # Creating new topic:
+    path("topics/new/", views.new_topic, name="new_topic"),
     # A single topic's page:
-    path("topics/<int:topic_id>/", views.topic, name="topic"),
+    path("topics/<int:topic_id>/entries", views.topic, name="topic"),
+    # Creating new entry:
+    path("topics/<int:topic_id>/entries/new", views.new_entry, name="new_entry"),
+    # Editing a previous entry:
+    path(
+        "topics/<int:topic_id>/entries/<int:entry_id>/",
+        views.edit_entry,
+        name="edit_entry",
+    ),
 ]
